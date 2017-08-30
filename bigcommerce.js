e98be2918993ca6657710dac7ecf138b91c3e8b3
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-08-30 18:51:47 -0500

    adding PUT,POST and DELETE methods

--- bigcommerce.js
+++ bigcommerce.js
@@ -19,7 +19,7 @@ Bigcommerce.time = (host, credentials) => {
 	return new Promise((resolve, reject) => {
 
 		options.method = 'GET';
-		options.path = options.path + 'time';
+		options.path = '/api/v2/' + 'time';
 		options.host = host;
 		options.headers.Authorization = credentials;
 
@@ -84,14 +84,18 @@ Bigcommerce.get = (host, credentials, params) => {
 	});       
 };
 //POST is work in progress
-Bigcommerce.post = (options, item, id, payload) => {
+Bigcommerce.put = (host, credentials, params, payload) => {
 	return new Promise((resolve, reject) => {
 
-		const new_path = options.path + item + '/' + id;
+		const new_path = '/api/v2/' + params;
 		options.path = new_path;
-		options.method = 'POST';
-
+		options.method = 'PUT';
+		options.host = host;
+		options.headers.Authorization = credentials;
+		payload = JSON.stringify(payload);
+	
 		const request = https.request(options, response => {
+			console.log(options);
 			let response_body = {
 				bigcommerce_return:''
 			};
@@ -105,14 +109,83 @@ Bigcommerce.post = (options, item, id, payload) => {
 				} catch (error) {
 					console.log('Cannot parse JSON');
 				}
+				console.log(response_body);
 				if (data) {
 					resolve(data);
 				} else {
 					reject('error occured');
 				}
 			});
-		}).end();
+		});
 		request.write(payload);
-	});       
+		request.end();
+	});    
 };
+
+Bigcommerce.post = (host, credentials, params, payload) => {
+	return new Promise((resolve, reject) => {
+
+		const new_path = '/api/v2/' + params;
+		options.path = new_path;
+		options.method = 'POST';
+		options.host = host;
+		options.headers.Authorization = credentials;
+		payload = JSON.stringify(payload);
+	
+		const request = https.request(options, response => {
+			console.log(options);
+			let response_body = {
+				bigcommerce_return:''
+			};
+			response.on('data', chunk => {
+				response_body.bigcommerce_return += chunk;
+			});
+			response.on('end', () => {
+				let data;
+				try {
+					data = JSON.parse(response_body.bigcommerce_return);
+				} catch (error) {
+					console.log('Cannot parse JSON');
+				}
+				console.log(response_body);
+				if (data) {
+					resolve(data);
+				} else {
+					reject('error occured');
+				}
+			});
+		});
+		request.write(payload);
+		request.end();
+	});    
+};
+
+Bigcommerce.delete = (host, credentials, params) => {
+	return new Promise((resolve, reject) => {
+
+		const new_path = '/api/v2/' + params;
+		options.path = new_path;
+		options.method = 'DELETE';
+		options.host = host;
+		options.headers.Authorization = credentials;
+	
+		const request = https.request(options, response => {
+			let response_body = {
+				bigcommerce_return:''
+			};
+			response.on('data', chunk => {
+				response_body.bigcommerce_return += chunk;
+			});
+			response.on('end', () => {
+				if (response.statusCode == 204) {
+					resolve(response.statusCode);
+				} else {
+					reject('error occured');
+				}
+			});
+		});
+		request.end();
+	});    
+};
+
 module.exports = Bigcommerce;
